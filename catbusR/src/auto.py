1f1042588badb4007b0a17669635a48556e2dc31
areophix <Morgan.deng@icloud.com>
2026-01-06 00:25:44 -0600

    little stuff

--- catbusR/src/auto.py
+++ catbusR/src/auto.py
@@ -10,11 +10,11 @@ def auto_drive(dir, pos):
         dif1 = pos-posY
         dif = dif1
         while math.fabs(dif) > 0.1:
-            spd_i = .393700787*inert.acceleration(ZAXIS)
-            drive_FB(dif/dif1) # percent of distance incompleted
+            acc_i = .393700787*inert.acceleration(ZAXIS)
+            drive_FB(dif/dif1) # percent of distance incompleted, 
             wait(10, MSEC)
-            spd_f = .393700787*inert.acceleration(ZAXIS) # convert to in
-            avg_spd = (spd_f-spd_i)/0.01 # find avg rate of change of acc
+            acc_f = .393700787*inert.acceleration(ZAXIS) # convert to in
+            avg_spd = (acc_f-acc_i)/0.01 # find avg rate of change of acc
             posY += avg_spd*0.01
             dif = pos-posY
     elif dir == "lr":
@@ -22,12 +22,11 @@ def auto_drive(dir, pos):
         dif1 = pos-posX
         dif = dif1
         while math.fabs(dif) > 0.1:
-            spd_i = .393700787*inert.acceleration(XAXIS)
+            acc_i = .393700787*inert.acceleration(XAXIS)
             drive_LR(dif/dif1)
             wait(10, MSEC)
-            spd_f = .393700787*inert.acceleration(XAXIS)
-            avg_spd = (spd_f-spd_i)/0.01
-            posX += avg_spd*0.01
+            acc_f = .393700787*inert.acceleration(XAXIS)
+            posX += acc_f-acc_i #i guess i literally couldve just simplified it to that whoops
             dif = pos-posX
     elif dir == "rot":
         dif1 = pos-inert.heading()
