6470f2f035e5951d564b74be9bb3d161a3e521eb
areophix <Morgan.deng@icloud.com>
2026-02-06 23:26:20 -0600

    pid auto drive?
    > i hope thats how it works
    > ports & bumpers to do
    > auton to do

--- catbusR/src/main.py
+++ catbusR/src/main.py
@@ -34,11 +34,11 @@ switch_cnt = 0
 def curve(x):
     return pow(x, 2)/100 * (x/abs(x)) * 0.25
 
-def drive(spdR, spdL, type = RPM):
-    LF.spin(FORWARD, curve(spdL), type)
-    LB.spin(FORWARD, curve(-spdL), type)
-    RF.spin(FORWARD, curve(spdR), type)
-    RB.spin(FORWARD, curve(-spdR), type)
+def drive(Rspd, Lspd, type):
+    LF.spin(FORWARD, Lspd, type)
+    LB.spin(FORWARD, -Lspd, type)
+    RF.spin(FORWARD, Rspd, type)
+    RB.spin(FORWARD, -Rspd, type)
 
 def belt(spd):
     belt1.spin(FORWARD, spd)
@@ -76,98 +76,63 @@ def manual_reset():
     pivot.reset_position()
     table.reset_position()
 
-# auto
-posY = 0.0
-posX = 0.0
+def auto_drive(target, max = 100):
+    LB.reset_position()
+    LF.reset_position()
+    RB.reset_position()
+    RF.reset_position()
+    # gains for pct/in although percent of 100 rpm literally is just rpm huh
+    p_g = 6
+    i_g = 0.01
+    d_g = 3.44
 
-# def auto_drive(dir, pos):
-#     t = 0
-#     if dir == "fb":
-#         global posY
-#         dif1 = pos-posY
-#         dif = dif1
-#         while math.fabs(dif) > 0.1 and t < dif1/24: #timeout, abt 24in/sec?
-#             acc_i = .393700787*inert.acceleration(XAXIS)
-#             drive_FB((dif/dif1)*100) # percent of distance incompleted, 
-#             print((dif/dif1)*100)
-#             wait(10, MSEC)
-#             acc_f = .393700787*inert.acceleration(XAXIS) # convert to in
-#             avg_spd = (acc_f-acc_i)/0.01 # find avg rate of change of acc
-#             posY += avg_spd*0.01
-#             dif = pos-posY
-#             t += 0.01
-#     elif dir == "lr":
-#         global posX
-#         dif1 = pos-posX
-#         dif = dif1
-#         while math.fabs(dif) > 0.1 and t < dif1/24:
-#             acc_i = .393700787*inert.acceleration(ZAXIS)
-#             drive_LR((dif/dif1)*100)
-#             wait(10, MSEC)
-#             acc_f = .393700787*inert.acceleration(ZAXIS)
-#             posX += acc_f-acc_i #i guess i literally couldve just simplified it to that whoops
-#             dif = pos-posX
-#             t += 0.01
-#     elif dir == "rot":
-#         dif1 = pos-inert.heading()
-#         dif = dif1
-#         while math.fabs(dif) > 0.1 and t < dif1/720: # 2 revs/sec?
-#             drive_rot((dif/dif1)*100)
-#             wait(10, MSEC)
-#             dif = pos-inert.heading()
-#             t += 0.01
-#     brake(BRAKE)
-#     belt_brake()
-#     wait(500, MSEC)
+    err = target
+    p_err = target
+    while err < 0.1:
+        curr = 3.25*(LB.position(TURNS) + LF.position(TURNS) + RB.position(TURNS) + RF.position(TURNS)) / 4
+        err = target - curr
+        p = p_g*err
+        i += i_g*err*100
+        d = d_g*(err-p_err) / 100
+
+        p_err = err
+        spd = p + i + d
+        if spd > max:
+            drive(max, max, RPM)
+        else:
+            drive(spd, spd, PERCENT)
+
+        wait(100, MSEC)
+    wait(300, MSEC)
+
+def auto_turn(target, max = 100):
+    inert.reset_rotation()
+    # gains for pct/in
+    p_g = 0.3
+    i_g = 0
+    d_g = 0.15
+
+    err = target
+    p_err = target
+    while err < 0.1:
+        curr = inert.rotation()
+        err = target - curr
+        p = p_g*err
+        i += i_g*err*100
+        d = d_g*(err-p_err) / 100
+
+        p_err = err
+        spd = p + i + d
+        if spd > max:
+            drive(-max, max, RPM)
+        else:
+            drive(-spd, spd, PERCENT)
+
+        wait(100, MSEC)
+    wait(300, MSEC)
 
 def autonomous():
-    auto_drive("fb", 24)
-    auto_drive("lr", -28)
-    auto_drive("fb", 15)
-    belt(-70)
-    # auto_drive("lr", -50)  # left by a chunk of range
-    # auto_drive("fb", 35)  # forward by a chunk of range
-    # belt(-70)  # who knows, just dump
-    # wait(900, MSEC)  # wait
-    # belt_brake()
-    # wait(1500, MSEC)  # score!
-
-def autonomous_old():
-    auto_drive("lr", -6)
-    belt(70)
-    auto_drive("fb", 48)
-    wait(750, MSEC) # wait for block collection?
-    belt_brake()
-
-    auto_drive("rot", 45)
-    pivot.spin_to_position(90, 40) #try spin_to_deg?
-    auto_drive("fb", 84)
-    belt(-70)
-    wait(1500, MSEC) #score 2?
-    belt_brake()
-    pivot.spin_to_position(0, 40)
-    auto_drive("fb", 48)
-
-    auto_drive("rot", 0)
-    auto_drive("lr", 54)
-    auto_drive("rot", 315)
-    auto_drive("fb", 84)
-    belt(-70)
-    wait(1500, MSEC) #score 2?
-    belt_brake()
-    auto_drive("fb", 48)
-
-    auto_drive("rot", 0)
-    auto_drive("fb", 36)
-    auto_drive("lr", -102)
-    pivot.spin_to_position(180)
-    auto_drive("fb", 12)
-    belt(-70)
-    wait(2, SECONDS)
-    belt_brake()
-
-    auto_drive("fb", 42)
-    belt(-70) #iunno just let it run until autons over?
+    
 
 def device_check():
     if LF.installed() and RF.installed() and LB.installed() and RB.installed():
@@ -223,7 +188,7 @@ def user_control():
         ax2 = controller.axis2.position()
 
         if abs(ax3) > 2 or abs(ax2) > 2:
-            drive(ax3, ax2)
+            drive(curve(ax3), curve(ax2), RPM)
         else:
             brake(BRAKE)
         wait(20, MSEC)
